dfbf8538f5ba90fac8855d2fb3cdb47bc3983883
dhanuprys <dhanuprys@gmail.com>
2024-07-30 21:21:06 +0800

    Order note by date

--- app/src/main/java/com/dedan/kalenderadat/data/NoteDao.kt
+++ app/src/main/java/com/dedan/kalenderadat/data/NoteDao.kt
@@ -9,10 +9,10 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
-    @Query("SELECT * FROM notes")
+    @Query("SELECT * FROM notes ORDER BY date DESC")
     fun getAllNotes(): Flow<List<NoteItem>>
 
-    @Query("SELECT * FROM notes WHERE date = :date LIMIT 1")
+    @Query("SELECT * FROM notes WHERE date = :date  LIMIT 1")
     fun getNoteByDateId(date: String): Flow<NoteItem?>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
